5a3ba3676420af5a0d0010c062f2f9a85f2db38d
GevorgDanielyan <gevorg.danielyan@outlook.com>
2023-04-26 21:55:00 +0400

    Added some files

---
+++ src/Infrastructure/Services/DbService.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using Npgsql;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Services
+{
+    internal sealed class DbService
+    {
+        private readonly IDbConnection _db;
+
+        public DbService(IConfiguration configuration)
+        {
+            _db = new NpgsqlConnection(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public async Task<T> GetAsync<T>(string command, object parms)
+        {
+            T result;
+
+            result = (await _db.QueryAsync<T>(command, parms).ConfigureAwait(false)).FirstOrDefault()!;
+
+            return result!;
+
+        }
+
+        public async Task<List<T>> GetAll<T>(string command, object parms)
+        {
+
+            List<T> result = new();
+
+            result = (await _db.QueryAsync<T>(command, parms)).ToList();
+
+            return result;
+        }
+
+        public async Task<int> EditData(string command, object parms)
+        {
+            int result;
+
+            result = await _db.ExecuteAsync(command, parms);
+
+            return result;
+        }
+    }
+}
